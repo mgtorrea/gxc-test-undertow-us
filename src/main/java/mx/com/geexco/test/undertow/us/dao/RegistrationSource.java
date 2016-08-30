49baa765cea807e60da54551a0ad778a8a9a64b6
Mario Alberto García <mgtorrea@users.noreply.github.com>
2016-08-30 11:45:23 -0500

    Update RegistrationSource.java

--- src/main/java/mx/com/geexco/test/undertow/us/dao/RegistrationSource.java
+++ src/main/java/mx/com/geexco/test/undertow/us/dao/RegistrationSource.java
@@ -5,12 +5,16 @@ import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 import mx.com.geexco.test.undertow.us.dto.RegistrationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author gxc-mg
  */
 public class RegistrationSource {
+    
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final Map<String, String> map = new HashMap<>();
 
@@ -25,7 +29,9 @@ public class RegistrationSource {
     public String []getDeviceIds(String[] ids) {
         List<String> devices=new ArrayList<String>();
         for(String id:ids){
+            log.info(id);
             String dev=map.get(id);
+            log.info(">>>"+dev);
             if(dev!=null){
                 devices.add(dev);
             }
